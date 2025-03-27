38e150dbcf68ef23d48a7e243b19938a84c01c7c
bbibbo1514 <gusdn1231234@gmail.com>
2025-03-27 11:12:28 +0900

    정리

--- Assets/_HW/ResourceTileSpawner.cs
+++ Assets/_HW/ResourceTileSpawner.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnvirmentManager : MonoBehaviour
+public class ResourceTileSpawner : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
